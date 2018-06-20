35a94190752203ba23d067735fe1f673dbfe3675
Aliza Rabinovitz <alizajessa24@gmail.com>
2018-06-20 17:33:09 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -13,7 +13,7 @@ def change_rainbow_colors
   rainbow_colors[0] = 'red'
   rainbow_colors[1] = 'light_red'
   rainbow_colors[2] = 'light_yellow'
-  puts rainbow_colors
+  return rainbow_colors
 end
 
 
@@ -25,5 +25,5 @@ def add_colors
   ]
   rainbow_colors.push("green")
   rainbow_colors.push("blue")
-  puts rainbow_colors
+  return rainbow_colors
 end
